75fcec10139ea7b3116efc1c7aa57f06258167bb
cxrvus <benito.m.rabe@gmail.com>
2024-03-14 22:44:18 +0100

    implemented loadDir

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello, world!');
+import * as fs from 'fs'
+
+export const loadDir = (dir: string) => {
+	return fs.readdirSync(dir)
+		// .filter((name) => !name.endsWith('.md'))
+		.map((name) => ({ 
+			file: getFileObj(name, `${dir}/${name}`)
+		}))
+	;
+}
+
+
+const getFileObj = (name: string, path: string) => {
+	return {
+		name,
+		path,
+		stats: fs.statSync(path),
+		rawContent: fs.readFileSync(path, 'utf-8'),
+	}
+}

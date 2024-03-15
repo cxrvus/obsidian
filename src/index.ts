3b17c28c826f940cdacab48bd8d2ef305e0d2177
cxrvus <benito.m.rabe@gmail,com>
2024-03-15 14:54:50 +0100

    progress

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,34 @@
 import * as fs from 'fs'
+import path = require('path');
 
 export const loadDir = (dir: string) => {
 	return fs.readdirSync(dir)
 		.filter((name) => name.endsWith('.md'))
-		.map((name) => ({ 
-			file: getFileObj(name, `${dir}/${name}`)
-		}))
+		.map((name) => getFileObj(dir, name))
 	;
 }
 
 
-const getFileObj = (name: string, path: string) => {
+const getFileObj = (dir: string, name: string) => {
+	const path = `${dir}/${name}`
+	const rawContent = fs.readFileSync(path, 'utf-8')
+
+	const lines = rawContent.split('\n')
+	const [first, second] = lines
+	const hasValidFrontMatter = first === '---' && second === '{'
+	const frontMatterEnd = lines.indexOf('---', 2)
+	const frontMatter = hasValidFrontMatter ? lines.slice(1, frontMatterEnd).join('\n') : '{}'
+	const content = hasValidFrontMatter ? lines.slice(frontMatterEnd + 1).join('\n') : rawContent
+	const attr = JSON.parse(frontMatter)
+	const attrEntires = Object.entries(attr)
+
 	return {
+		attr,
+		attrEntires,
+		content,
 		name,
 		path,
+		rawContent,
 		stats: fs.statSync(path),
-		rawContent: fs.readFileSync(path, 'utf-8'),
-	}
+	};
 }

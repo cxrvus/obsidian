ac28e3b6790b7d293d9d4f01c395c06176c9e95e
cxrvus <benito.m.rabe@gmail,com>
2024-04-23 19:30:04 +0200

    misc

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -2,5 +2,5 @@ import { DataviewInlineApi } from '../../lib/dv-types/api/inline-api'
 
 
 export const today = (dv: DataviewInlineApi) => dv.date('today')
-export const oneMonthAgo = (dv: DataviewInlineApi) => today(dv).minus(dv.duration('1mo'))
-export const future = (dv: DataviewInlineApi) => today(dv).plus(dv.duration('3mo'))
+export const oneMonthAgo = (dv: DataviewInlineApi) => today(dv).minus(dv.duration('1 mo'))
+export const future = (dv: DataviewInlineApi) => today(dv).plus(dv.duration('3 mo'))

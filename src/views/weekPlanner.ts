7e506bc7f1b72f94842003a2715f2f2a4d976733
cxrvus <benito.m.rabe@gmail.com>
2024-04-23 22:20:28 +0200

    added total weekly duration

--- src/views/weekPlanner.ts
+++ src/views/weekPlanner.ts
@@ -9,7 +9,9 @@ export default (dv: DataviewInlineApi) => {
 	// @ts-expect-error cannot safely type due, will deprecate luxon or fix this later
 	const groupedTasks = upcomingTasks.groupBy(x => x.due.toISODate())
 	const summedUpTasks = groupedTasks.map(({key, rows}) => [key, rows.length, durationSum(dv, rows)])
+	const totalDuration = durationSum(dv, upcomingTasks)
 
 	dv.header(2, 'Durations')
+	dv.paragraph(`Total duration: ${totalDuration}`)
 	dv.table(['Date', 'Tasks', 'Duration'], summedUpTasks)
 }

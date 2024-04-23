1e0c8890f1ef7ab21df227285c72b0f3aaf64e8f
cxrvus <benito.m.rabe@gmail.com>
2024-04-23 22:16:28 +0200

    renamed variables

--- src/views/weekPlanner.ts
+++ src/views/weekPlanner.ts
@@ -7,8 +7,9 @@ export default (dv: DataviewInlineApi) => {
 	const upcomingTasks = getDueTasks(dv).filter(x => x.due > today(dv) && x.due <= inOneWeek(dv))
 
 	// @ts-expect-error cannot safely type due, will deprecate luxon or fix this later
-	const foo = upcomingTasks.groupBy(x => x.due.toISODate())
-	const bar = foo.map(({key, rows}) => [key, rows.length, durationSum(dv, rows)])
+	const groupedTasks = upcomingTasks.groupBy(x => x.due.toISODate())
+	const summedUpTasks = groupedTasks.map(({key, rows}) => [key, rows.length, durationSum(dv, rows)])
 
-	dv.table(['Date', 'Tasks', 'Duration'], bar)
+	dv.header(2, 'Durations')
+	dv.table(['Date', 'Tasks', 'Duration'], summedUpTasks)
 }
